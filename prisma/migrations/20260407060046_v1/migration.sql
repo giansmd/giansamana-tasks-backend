75347d17cab875815f959265edeb9c0ead9324be
Gian Franco Samana <gian.samana.1801@gmail.com>
2026-04-07 01:10:01 -0500

    feat: initialize backend project with Prisma and PostgreSQL setup
    - Add package.json with dependencies and devDependencies for TypeScript, Prisma, and PostgreSQL.
    - Create prisma.config.ts for Prisma configuration.
    - Define initial migration to create Task table.
    - Update migration to drop Task table and create blocks, projects, and areas tables with foreign key relationships.
    - Add migration lock file for version control.
    - Define Prisma schema with models for Block, Project, and Area.
    - Add tsconfig.json for TypeScript configuration with strict type checking.

---
+++ prisma/migrations/20260407060046_v1/migration.sql
@@ -0,0 +1,47 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Task` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "Task";
+
+-- CreateTable
+CREATE TABLE "blocks" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "completed" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "project_id" TEXT NOT NULL,
+
+    CONSTRAINT "blocks_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "projects" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "area_id" TEXT NOT NULL,
+
+    CONSTRAINT "projects_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "areas" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "areas_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "blocks" ADD CONSTRAINT "blocks_project_id_fkey" FOREIGN KEY ("project_id") REFERENCES "projects"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "projects" ADD CONSTRAINT "projects_area_id_fkey" FOREIGN KEY ("area_id") REFERENCES "areas"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
